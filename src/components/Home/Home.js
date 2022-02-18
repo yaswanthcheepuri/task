1b8592ba11c3250efe2c3054f2e37efbeead863d
sriram <kvkrcsetty@gmail.com>
2022-02-18 17:40:07 +0530

    Home page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,9 +10,28 @@ function Home(){
                  <h1 >Amazon</h1>
                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
                      
+                 <h3 className="p-2">login</h3>
+                    <h3>singup</h3>
                  </div>
                  </div>
              </nav>
+
+             <h3 className='fw-bold mb-3'>New products</h3>
+
+             <div className='row text-center'>
+                 <div className='bg-light col-sm-3'>
+                     <img style={{width:"10rem"}} src='https://m.media-amazon.com/images/I/51IrSQmuLgS._AC_SR360,240_QL70_.jpg'></img>
+                     <h6 className='fw-bold'>$2000</h6>
+                     <h6>Double door refrigerator</h6>
+                 </div>
+             </div>
+             <div className='row text-center'>
+                 <div className='bg-light col-md-3'>
+                     <img style={{width:"10rem"}} src='https://m.media-amazon.com/images/I/61cgN3yhH-L._AC_UY327_FMwebp_QL65_.jpg'></img>
+                     <h6 className='fw-bold'>$1000</h6>
+                     <h6> Single door refrigerator</h6>
+                 </div>
+             </div>
            
         </div>
     );
