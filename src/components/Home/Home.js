ada4c661d9f1bbd62326b8877465814205f1cd63
sriram <kvkrcsetty@gmail.com>
2022-02-18 16:40:17 +0530

    initial app

---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+//import React from 'react'
+import './Home.css'
+
+
+function Home(){
+    return(
+        <div>
+             <nav className="navbar navbar-expand navbar-bark bg-white text-back ">
+                 <div className='container fluid'>
+                 <h1 >Amazon</h1>
+                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                     
+                 </div>
+                 </div>
+             </nav>
+           
+        </div>
+    );
+}
+export default Home
